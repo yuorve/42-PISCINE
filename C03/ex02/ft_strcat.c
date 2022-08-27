eeaad20a286600b8f822f3f56efa76b31189236f
Yury Oropeza <yuryoropeza@hotmail.com>
2022-08-27 15:42:56 +0000

    Dia 20

--- C03/ex02/ft_strcat.c
+++ C03/ex02/ft_strcat.c
@@ -10,6 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <string.h>
+
 char	*ft_strcat(char *dest, char *src)
 {
 	int	i;
@@ -26,4 +29,18 @@ char	*ft_strcat(char *dest, char *src)
 	}
 	dest[i + j] = '\0';
 	return (dest);
+}
+
+int main(int argv, char **argc)
+{
+	char *mine;
+	char *theirs;
+
+	if (argv == 3)
+	{
+		mine = ft_strcat(argc[1], argc[2]);
+		theirs = strcat(argc[1], argc[2]);
+		printf("Mi resultado: %s, El suyo: %s", mine, theirs);
+	}
+	return (0);
 }
