9493369a3051936a11770ecef095a48d25bbae10
Yury Oropeza <yuryoropeza@hotmail.com>
2022-08-29 14:40:42 +0000

    Dia 22

---
+++ C04/ex04/ft_putnbr_base.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoropeza <yoropeza@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/28 18:17:32 by yoropeza          #+#    #+#             */
+/*   Updated: 2022/08/28 19:50:58 by yoropeza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(int n, char *base)
+{
+	write(1, &base[n], 1);
+}
+
+int ft_dup_char(char *base)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (base[i] != '\0')
+    {
+        j = 0;
+        while (base[j] != '\0')
+        {
+            if (j != i && base[j] == base[i])
+                return (1);
+        }
+    }
+    return (0);
+}
+
+int ft_check_base(char *base)
+{
+    int len;
+
+    len = 0;
+    while (*base++)
+        len++;
+    if (ft_dup_char(base) == 1)
+        return (0);
+    if (base[0] == '\0' || base[1] == '\0')
+        return (0);
+    while (*base++)
+        if (*base == '-' || *base == '+')
+            return (0);
+    return (len);
+}
+
+void    ft_putnbr_base(int nbr, char *base)
+{
+    int size_base;
+
+    if ((size_base = ft_check_base(base)) == 0)
+    {
+        write(1, '\0', 1);
+    }
+	else if (nbr == -2147483648)
+	{        
+        ft_putnbr_base(214748364 / size_base, base);
+        ft_putnbr_base(8 / size_base, base);
+    }
+    else if (nbr < 0)
+	{
+		write(1, "-", 1);
+		nbr = -nbr;
+	}
+    else if (nbr >= 10)
+	{
+		ft_putnbr_base(nbr / size_base, base);
+        ft_putnbr_base(nbr % size_base, base);
+		//nbr = nbr % size_base;
+	}
+	else (nbr < 10)
+		ft_putchar(nbr, base);
+}
---
+++ C04/ex05/ft_atoi_base.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoropeza <yoropeza@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/28 18:17:32 by yoropeza          #+#    #+#             */
+/*   Updated: 2022/08/28 19:50:58 by yoropeza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(int n, char *base)
+{
+	write(1, &base[n], 1);
+}
+
+int ft_dup_char(char *base)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (base[i] != '\0')
+    {
+        j = 0;
+        while (base[j] != '\0')
+        {
+            if (j != i && base[j] == base[i])
+                return (1);
+        }
+    }
+    return (0);
+}
+
+int ft_check_base(char *base)
+{
+    int len;
+
+    len = 0;
+    while (*base++)
+        len++;
+    if (ft_dup_char(base) == 1)
+        return (0);
+    if (base[0] == '\0' || base[1] == '\0')
+        return (0);
+    while (*base++)
+        if (*base == '-' || *base == '+')
+            return (0);
+    return (len);
+}
+
+int ft_atoi_base(char *str, char *base)
+{
+	int	i;
+	int	number;
+	int	negative;
+    int size_base;
+
+    if ((size_base = ft_check_base(base)) == 0)
+    {
+        return (0);
+    }
+    else 
+    {
+        i = 0;
+        number = 0;
+        negative = 1;
+        while (*str == 32 || (*str >= 9 && *str <= 13))
+            str++;
+        while (*str == '-' || *str == '+')
+        {
+            if (*str == '-')
+                negative *= -1;
+            str++;
+        }
+        while (*str >= '0' && *str <= '9')
+        {
+            number = number * size_base;
+            number = number + base[(str[i] - '0')];
+            str++;
+        }
+        return (number * negative);
+    }
+}
